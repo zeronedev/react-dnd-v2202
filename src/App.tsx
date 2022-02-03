afe43ec26d5818f03013963238bfe99c8c47ff6b
zeronedev <ok7375@gmail.com>
2022-02-03 18:08:26 +0900

    #6.2 Drag and Drop part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 function App() {
-  return null;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
